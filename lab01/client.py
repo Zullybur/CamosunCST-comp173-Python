66c246484de571c3f7d8bec513179183410eeb7d
Zullybur <mattcasiro@gmail.com>
2016-04-13 11:46:49 -0700

    client submission code complete

--- lab01/client.py
+++ lab01/client.py
@@ -10,17 +10,36 @@ import socket
 import sys
 
 if __name__ == '__main__':
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     address = (sys.argv[1], int(sys.argv[2]))
     b = bytearray()
-    i = 0
 
-    for v in sys.argv:
-        if (i > 2):
-            b.append(int(sys.argv[i]))
-        i+=1
+    # Add operator code
+    if sys.argv[3] == '+':
+        b.append(2**0)
+    elif sys.argv[3] == '-':
+        b.append(2**1)
+    elif sys.argv[3] == '*':
+        b.append(2**2)
+    else:
+        b.append(0)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Check paramater list count
+    if len(sys.argv) > 15:
+        maxRange = 15
+    else:
+        maxRange = len(sys.argv)
+    # Add parameters
+    for i in range(4, maxRange):
+        if i % 2 == 0:
+            b.append((int(sys.argv[i])) << 4)
+        else:
+            b[(i-3)//2] = b[(i-3)//2] | int(sys.argv[i])
+    print (b)
+    # Connect to server, send data, and receive response
     s.connect(address)
     s.sendall(b)
-    result = s.recv(4096)
-    print (int(result[0]))
+    data = s.recv(4096)
+    # Process and output result
+    result = (int(data[0]) << 8) + int(data[1])
+    print (result)
