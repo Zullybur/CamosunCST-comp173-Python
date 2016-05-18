eb64551bd2f4aec89b1dc60460c340555205ca57
Zullybur <mattcasiro@gmail.com>
2016-05-18 12:15:39 -0700

    basic communication established - need to move to state based system

--- lab03/client.py
+++ lab03/client.py
@@ -1,54 +1,108 @@
-import sys
+import sys, socket
 
 verbose = "-v" in sys.argv
+debug = "-d" in sys.argv
+
+# if verbose: print("")
+# if debug: print("")
 
 # 
 def makeTCPConnection(address):
+    if debug: print("Making connection")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(address)
-    checkResponse(s)
+    if debug: print("Connection made")
     return s
 # 
-def checkResponse(s):
+def checkResponse(s, expected):
     response = s.recv(1024).decode("UTF-8")
-    if response != "READY":
+
+    if response != expected:
         if verbose: print ("Server Error!", response)
-        sys.exit(1)
+        return False
+
     if verbose: print ("Server response:", response)
-# 
-def requestData():
-    pass
-# Searches for 'term' in 'data' and clears text before if 'position' = "head" or after if 'position' = "tail"
-def cleanData(data, term, position):
-    pos = data.upper.find(term)
+    return True
+
+# Request a resource from a web server
+# Pre: there is a TCP connection made with a webserver
+# Post: the socket is receiving the resource to the buffer
+# Param s: is a socket object
+# Param uri: is a resource string to be requested from the web server, begining with "/"
+def getHTML(s, uri):
+    request = "GET "+uri+"\n\n"
+    encodedRequest = request.encode("UTF-8")
+    if debug: print("Making GET request")
+    s.send(encodedRequest)
+
+# Searches for 'term' in 'data' and truncates data 'beg'ining (before term) or 'end' (after term)
+def cleanData(data, term, trunc):
+    pos = data.upper().find(term.upper())
     if pos == -1:
-        return false, data
-    elif position = "head":
-        return true, data[pos:]
-    elif position = "tail":
-        return true, data[:(pos + len(term)]
+        return False, "empty"
+    elif trunc == "beg":
+        return True, data[pos:]
+    elif trunc == "end":
+        return True, data[:(pos + len(term))]
+
 #
 def sendData():
     pass
 
+# Extract the program varaibles from the parameter
+# Pre: N/A
+# Post: N/A
+# Param raw: a full web address begining with "http://"
+def extractParams(raw):
+    ignoredChars = 7
+    i = raw.find("/", ignoredChars)
+    host = raw[ignoredChars:i] if i > 0 else raw[ignoredChars:]
+    resource = raw[i:] if i > 0 else "/"
+    if verbose: print ("'extractParams' executed:\nraw: {}, i: {}, host: {}, resource: {}" .format(raw, i, host, resource))
+    return host, resource
+
+# Run main
 if __name__ == '__main__':
-    source = makeTCPConnection()
+    host, resource = extractParams(sys.argv[1])
+    source = makeTCPConnection( (host, 80) )
+    getHTML(source, resource)
+    
+    state = 1
+    data = ""
+    foundOpenHTML
+    foundCloseHTML
+    # Cycle states until all processes are finnished
+    while state != 4:
+        # Get data from web server
+        if state == 1:
+            clean, found = cleanData(source.recv(1024).decode("UTF-8"))
+        elif state == 2:
+            pass
+        elif state == 3:
+            pass
+
     # Pull data until the opening <HTML> tag is found
     proceed = False
-    data = ""
     while not proceed:
-        data += requestData(source)
-        proceed, cleaned = cleanData(data, "<HTML>", "head")
-        # Check for matching closing tag in same data block
-        if proceed:
-            oneblock, cleaned = cleanData(cleaned, "</HTML>", "tail")
-    # Handle the remaining data in the initial parse
-    parser = makeConnection(("rtvm.cs.camosun.bc.ca", 10010))
+        data = 
+        proceed, cleaned = cleanData(data, "<HTML>", "beg")
+        # if debug: print (cleaned)
+    if verbose: print ("'<HTML>' found")
+    
+    # Check for matching closing tag in same data block
+    oneblock, cleaned = cleanData(cleaned, "</HTML>", "end")
+    if verbose and oneblock: print ("'</HTML>' found in first block")
+
+    parser = makeTCPConnection( ("rtvm.cs.camosun.bc.ca", 10010) )
+    if not checkResponse(parser, "READY"): sys.exit()
     if oneblock:
-        pass
+        if debug: print ("sending:", cleaned)
+        parser.send(cleaned.encode("UTF-8"))
+        data = parser.recv(1024).decode("UTF-8")
+        print (data)
     else:
         proceed = False
         while not proceed:
-            data = requestData(source)
+            data = requestHTTPData(source)
             proceed, cleaned = cleanData(data)
             sendData(parser, cleaned)
